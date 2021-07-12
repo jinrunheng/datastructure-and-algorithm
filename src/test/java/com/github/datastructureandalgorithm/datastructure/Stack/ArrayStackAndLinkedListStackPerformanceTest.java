ad660960a02901167f8d3a94b1fca2eec1a00de1
jinrunheng <1175088275@qq.com>
2021-07-12 22:29:39 +0800

    finish ArrayStack & LinkedListStack performance test

---
+++ src/test/java/com/github/datastructureandalgorithm/datastructure/Stack/ArrayStackAndLinkedListStackPerformanceTest.java
@@ -0,0 +1,34 @@
+package com.github.datastructureandalgorithm.datastructure.Stack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class ArrayStackAndLinkedListStackPerformanceTest {
+
+    private static double testStack(Stack<Integer> stack, int opCount) {
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            stack.push(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            stack.pop();
+        }
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+
+    }
+
+    @Test
+    void test() {
+        int opCount = 100000;
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack,time: " + time1 + " s");
+
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack,time: " + time2 + " s");
+    }
+}
