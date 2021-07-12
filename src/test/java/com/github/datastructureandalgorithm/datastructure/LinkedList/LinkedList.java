f030029aefb9abeb348f4ab8d4ebde016060adc5
jinrunheng <1175088275@qq.com>
2021-07-12 21:08:58 +0800

    finish LinkedList add function

--- src/test/java/com/github/datastructureandalgorithm/datastructure/LinkedList/LinkedList.java
+++ src/test/java/com/github/datastructureandalgorithm/datastructure/LinkedList/LinkedList.java
@@ -24,4 +24,63 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedList() {
+        dummyHead = new Node();
+        size = 0;
+    }
+
+    /**
+     * @return 链表中元素的个数
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @return 当前链表是否为空
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * @param e     向链表中插入的元素
+     * @param index 向链表中插入的位置（索引）
+     */
+    public void add(E e, int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Insert failed.Index is illegal.");
+        }
+        Node pre = dummyHead;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        pre.next = new Node(e, pre.next);
+        size++;
+    }
+
+    /**
+     * 在链表头部添加新的元素 e
+     *
+     * @param e 添加的元素
+     */
+    public void addFirst(E e) {
+        add(e, 0);
+    }
+
+    /**
+     *
+     * 在链表尾部添加新的元素 e
+     *
+     * @param e 添加的元素
+     */
+    public void addLast(E e) {
+        add(e, size);
+    }
+
+
 }
