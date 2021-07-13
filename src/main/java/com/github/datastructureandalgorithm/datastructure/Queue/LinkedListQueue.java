956e094ee7ee322202d2dcc3d73fb0d56abeb26d
jinrunheng <1175088275@qq.com>
2021-07-14 06:26:25 +0800

    finish LinkedListQueue

---
+++ src/main/java/com/github/datastructureandalgorithm/datastructure/Queue/LinkedListQueue.java
@@ -0,0 +1,96 @@
+package com.github.datastructureandalgorithm.datastructure.Queue;
+
+public class LinkedListQueue<E> implements Queue<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LinkedListQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public void enqueue(E e) {
+        if (tail == null) {
+            head = new Node(e);
+            tail = head;
+
+        } else {
+            tail.next = new Node(e);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Dequeue failed.Queue is empty!");
+        }
+        Node ret = head;
+        head = head.next;
+        ret.next = null;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return ret.e;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new RuntimeException("Queue is empty!");
+        }
+        return head.e;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Queue：front");
+        Node cur = head;
+        while (cur != null) {
+            sb.append(cur + "->");
+            cur = cur.next;
+        }
+        sb.append("NULL tail");
+        return sb.toString();
+    }
+}
