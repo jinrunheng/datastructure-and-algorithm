af2d4f828dce48b5795c13695182b6d57a6a75ec
jinrunheng <1175088275@qq.com>
2021-07-17 23:18:39 +0800

    finish BST tests

--- src/test/java/com/github/datastructureandalgorithm/datastructure/BST/BSTTest.java
+++ src/test/java/com/github/datastructureandalgorithm/datastructure/BST/BSTTest.java
@@ -1,6 +1,7 @@
 package com.github.datastructureandalgorithm.datastructure.BST;
 
 import com.github.datastructureandalgorithm.algorithm.util.TreeUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,18 +10,53 @@ class BSTTest {
 
     @Test
     void size() {
+        BST<Integer> bst = new BST<>();
+        Assertions.assertEquals(bst.size(), 0);
+        bst.add(1);
+        Assertions.assertEquals(bst.size(), 1);
     }
 
     @Test
     void isEmpty() {
+        BST<Integer> bst = new BST<>();
+        Assertions.assertTrue(bst.isEmpty());
     }
 
     @Test
-    void add() {
+    void getRoot() {
+        BST<Integer> bst = new BST<>();
+        //    11
+        //   /  \
+        //  7    14
+        // / \   / \
+        //5  9  12  16
+        bst.add(11);
+        bst.add(7);
+        bst.add(14);
+        bst.add(5);
+        bst.add(9);
+        bst.add(12);
+        bst.add(16);
+        Assertions.assertEquals(bst.getRoot().e, 11);
     }
 
     @Test
     void contains() {
+        //    11
+        //   /  \
+        //  7    14
+        // / \   / \
+        //5  9  12  16
+        BST<Integer> bst = new BST<>();
+        bst.add(11);
+        bst.add(7);
+        bst.add(14);
+        bst.add(5);
+        bst.add(9);
+        bst.add(12);
+        bst.add(16);
+        Assertions.assertTrue(bst.contains(12));
+        Assertions.assertFalse(bst.contains(15));
     }
 
     @Test
