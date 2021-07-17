977eafa32075f08c4a2bce2e26aba55ef382396b
jinrunheng <1175088275@qq.com>
2021-07-17 21:15:10 +0800

    finish BST Constructor

---
+++ src/main/java/com/github/datastructureandalgorithm/datastructure/BST/BST.java
@@ -0,0 +1,33 @@
+package com.github.datastructureandalgorithm.datastructure.BST;
+
+public class BST<E extends Comparable<E>> {
+
+    private class Node {
+        public E e;
+        public Node left;
+        public Node right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+}
