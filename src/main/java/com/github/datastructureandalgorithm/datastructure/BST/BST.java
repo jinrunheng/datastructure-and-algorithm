87b206446501b534a46082e9cc187d28d8f0b7cb
jinrunheng <1175088275@qq.com>
2021-07-17 21:57:24 +0800

    finish add Node to BST

--- src/main/java/com/github/datastructureandalgorithm/datastructure/BST/BST.java
+++ src/main/java/com/github/datastructureandalgorithm/datastructure/BST/BST.java
@@ -30,4 +30,41 @@ public class BST<E extends Comparable<E>> {
         return size == 0;
     }
 
+    /**
+     * @param e 向二分搜索树中添加新的元素
+     */
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(e, root);
+        }
+    }
+
+    /**
+     * @param e    向二分搜索树中添加新的元素
+     * @param node 当前的比较的节点
+     */
+    private void add(E e, Node node) {
+
+        // if e < root.e,then go left;else go right
+        if (e.compareTo(node.e) == 0) {
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            add(e, node.left);
+        } else {
+            add(e, node.right);
+        }
+    }
 }
