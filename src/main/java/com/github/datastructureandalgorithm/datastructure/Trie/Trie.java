9cd38028c1bf9063832c62781dc5596e9641f4e2
jinrunheng <1175088275@qq.com>
2021-07-30 22:37:45 +0800

    finish Trie match method

--- src/main/java/com/github/datastructureandalgorithm/datastructure/Trie/Trie.java
+++ src/main/java/com/github/datastructureandalgorithm/datastructure/Trie/Trie.java
@@ -2,6 +2,7 @@ package com.github.datastructureandalgorithm.datastructure.Trie;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * 实现的 Trie 默认不会添加重复的单词
@@ -110,4 +111,40 @@ public class Trie {
     public boolean isPrefix(String prefix) {
         return prefix(prefix) > 0;
     }
+
+    /**
+     * 匹配查询：
+     * <p>
+     * 符号 "." 可以匹配任何字符
+     * 例如输入 word = d..r
+     * <p>
+     * 如果 Trie 中包含字符串 deer，那么 d..r 就可以匹配到 deer，结果就会返回 true
+     *
+     * @param word
+     * @return
+     */
+    public boolean match(String word) {
+        return match(root, word, 0);
+    }
+
+    private boolean match(TrieNode node, String word, int index) {
+        if (index == word.length()) {
+            return node.isWord;
+        }
+        char c = word.charAt(index);
+        if (c != '.') {
+            if (node.next.get(c) == null) {
+                return false;
+            }
+            return match(node.next.get(c), word, index + 1);
+        } else {
+            for (char nextChar : node.next.keySet()) {
+                if (match(node.next.get(nextChar), word, index + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
 }
