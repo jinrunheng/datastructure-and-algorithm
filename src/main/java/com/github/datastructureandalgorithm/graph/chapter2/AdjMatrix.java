44addbd57e9dab37f6be38254ebae4f941be0285
jinrunheng <1175088275@qq.com>
2021-08-06 06:51:11 +0800

    add method adj & degree

--- src/main/java/com/github/datastructureandalgorithm/graph/chapter2/AdjMatrix.java
+++ src/main/java/com/github/datastructureandalgorithm/graph/chapter2/AdjMatrix.java
@@ -2,6 +2,8 @@ package com.github.datastructureandalgorithm.graph.chapter2;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -20,11 +22,21 @@ public class AdjMatrix {
 
         try (Scanner scanner = new Scanner(file)) {
             V = scanner.nextInt();
+            if (V < 0) throw new IllegalArgumentException("V must be non-negative");
+
             adj = new int[V][V];
             E = scanner.nextInt();
+            if (E < 0) throw new IllegalArgumentException("E must be non-negative");
+
             for (int i = 0; i < E; i++) {
                 int a = scanner.nextInt();
+                validateVertex(a);
                 int b = scanner.nextInt();
+                validateVertex(b);
+
+                if (a == b) throw new IllegalArgumentException("Self Loop is detected");
+                if (adj[a][b] == 1) throw new IllegalArgumentException("Parallel Edges are detected");
+
                 adj[a][b] = 1;
                 adj[b][a] = 1;
             }
@@ -32,6 +44,58 @@ public class AdjMatrix {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex" + v + "is invalid");
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public boolean hasEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        return adj[v][w] == 1;
+    }
+
+    /**
+     * 返回和 v 顶点相邻的顶点
+     *
+     * @param v
+     * @return
+     */
+    public List<Integer> adj(int v) {
+        validateVertex(v);
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            if (adj[v][i] == 1) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 返回一个顶点的度（度的概念就是一个顶点有多少条邻边）
+     *
+     * @return
+     */
+    public int degree(int v) {
+        validateVertex(v);
+        int res = 0;
+        for (int i = 0; i < V; i++) {
+            if (adj[v][i] == 1) {
+                res++;
+            }
+        }
+        return res;
     }
 
     @Override
