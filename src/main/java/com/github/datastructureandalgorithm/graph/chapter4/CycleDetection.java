20f64b1377adbb530eee22914f6f1635e3b4473a
jinrunheng <1175088275@qq.com>
2021-08-10 12:03:54 +0800

    improve CycleDetection

--- src/main/java/com/github/datastructureandalgorithm/graph/chapter4/CycleDetection.java
+++ src/main/java/com/github/datastructureandalgorithm/graph/chapter4/CycleDetection.java
@@ -15,25 +15,31 @@ public class CycleDetection {
         visited = new boolean[G.V()];
         for (int v = 0; v < G.V(); v++)
             if (!visited[v])
-                dfs(v, v);
+                if (dfs(v, v)) {
+                    hasCycle = true;
+                    break;
+                }
     }
 
     /**
-     * 对图进行深度优先遍历.
+     * 从顶点 v 开始，判断图中是否有环
      * <p>
      * 传入参数的含义，v 为遍历到的顶点，parent 为  v 顶点是从哪一个顶点遍历而来的
      *
      * @param v
      */
-    private void dfs(int v, int parent) {
+    private boolean dfs(int v, int parent) {
         visited[v] = true;
 
         for (int w : G.adj(v)) {
-            if (!visited[w])
-                dfs(w, v);
-            else if (w != parent)
-                hasCycle = true;
+            if (!visited[w]) {
+                if (dfs(w, v)) {
+                    return true;
+                }
+            } else if (w != parent)
+                return true;
         }
+        return false;
     }
 
     /**
