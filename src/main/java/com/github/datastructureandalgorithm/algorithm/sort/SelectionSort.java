a6ae99808d2af83e328ac547729d62ef56f91097
jinrunheng <1175088275@qq.com>
2021-08-12 16:41:30 +0800

    update SelectionSort

--- src/main/java/com/github/datastructureandalgorithm/algorithm/sort/SelectionSort.java
+++ src/main/java/com/github/datastructureandalgorithm/algorithm/sort/SelectionSort.java
@@ -4,15 +4,15 @@ import com.github.datastructureandalgorithm.algorithm.util.SwapUtils;
 
 public class SelectionSort {
     public static void sort(int[] arr) {
-        if (arr == null || arr.length <= 1) {
+        if (arr == null || arr.length < 2)
             return;
-        }
+
         int minIndex;
         for (int i = 0; i < arr.length; i++) {
             minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++)
                 minIndex = arr[minIndex] > arr[j] ? j : minIndex;
-            }
+
             SwapUtils.swap(arr, minIndex, i);
         }
     }
