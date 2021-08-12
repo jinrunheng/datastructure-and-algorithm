c41d3533afd985eda756d39fb6bac49a2b12a258
jinrunheng <1175088275@qq.com>
2021-08-12 11:58:43 +0800

    add LinearSearch & LinearSearch Test

--- src/test/java/com/github/datastructureandalgorithm/algorithm/LinearSearch/LinearSearchTest.java
+++ src/test/java/com/github/datastructureandalgorithm/algorithm/LinearSearch/LinearSearchTest.java
@@ -1,5 +1,6 @@
 package com.github.datastructureandalgorithm.algorithm.LinearSearch;
 
+import com.github.datastructureandalgorithm.algorithm.util.ArrayGenerator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,14 +12,21 @@ class LinearSearchTest {
     void search() {
         String[] strings = {"A", "B", "C", "D", "E"};
         int index = LinearSearch.search(strings, "B");
-        Assertions.assertEquals(index,1);
+        Assertions.assertEquals(index, 1);
     }
 
     @Test
-    void performanceTest(){
-        int[] dataSize = {100_0000,1000_0000};
-        for(int size : dataSize){
+    void performanceTest() {
+        int[] dataSize = {100_0000, 1000_0000};
+        for (int size : dataSize) {
+            Integer[] testData = ArrayGenerator.generateOrderedArray(size);
+            long start = System.nanoTime();
 
+            for (int i = 0; i < 10; i++)
+                LinearSearch.search(testData, size);
+
+            long end = System.nanoTime();
+            System.out.println("t : " + (end - start) / 1000000000.0 + "s");
         }
     }
 }
