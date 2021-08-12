3f72bbfac4f3f408aba43772b21fdef420e3ac97
jinrunheng <1175088275@qq.com>
2021-08-12 17:05:54 +0800

    use generic

--- src/main/java/com/github/datastructureandalgorithm/algorithm/sort/BubbleSort.java
+++ src/main/java/com/github/datastructureandalgorithm/algorithm/sort/BubbleSort.java
@@ -4,17 +4,15 @@ import com.github.datastructureandalgorithm.algorithm.util.SwapUtils;
 
 public class BubbleSort {
 
-    public static void sort(int[] arr) {
-        if (arr == null || arr.length <= 1) {
-            return;
-        }
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    SwapUtils.swap(arr, j, j + 1);
-                }
-            }
-        }
-    }
+    private BubbleSort(){}
 
+    public static <T extends Comparable> void sort(T[] arr) {
+        if (arr == null || arr.length <= 1)
+            return;
+
+        for (int i = 0; i < arr.length - 1; i++)
+            for (int j = 0; j < arr.length - 1 - i; j++)
+                if (arr[j].compareTo(arr[j + 1]) > 0)
+                    SwapUtils.swap(arr, j, j + 1);
+    }
 }
---
+++ src/test/java/com/github/datastructureandalgorithm/algorithm/sort/BubbleSortTest.java
@@ -0,0 +1,22 @@
+package com.github.datastructureandalgorithm.algorithm.sort;
+
+import com.github.datastructureandalgorithm.algorithm.util.ArrayUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+
+    @Test
+    void sort() {
+        Integer[] randomArray = ArrayUtils.generateRandomArray(100, 100);
+        Integer[] copyArray = ArrayUtils.copyArray(randomArray);
+        Arrays.sort(randomArray);
+        BubbleSort.sort(copyArray);
+        for(int i = 0; i < randomArray.length; i++)
+            Assertions.assertEquals(randomArray[i],copyArray[i]);
+    }
+}
