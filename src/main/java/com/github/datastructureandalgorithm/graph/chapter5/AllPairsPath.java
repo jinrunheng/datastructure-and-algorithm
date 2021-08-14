3283aaf2d7bb0b945a46f6e85eff58e0d08be34c
jinrunheng <1175088275@qq.com>
2021-08-14 13:44:44 +0800

    finish BFS AllPairsPath

---
+++ src/main/java/com/github/datastructureandalgorithm/graph/chapter5/AllPairsPath.java
@@ -0,0 +1,39 @@
+package com.github.datastructureandalgorithm.graph.chapter5;
+
+public class AllPairsPath {
+    private Graph G;
+    private SingleSourcePath[] paths;
+
+    public AllPairsPath(Graph G) {
+        this.G = G;
+        paths = new SingleSourcePath[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            paths[v] = new SingleSourcePath(G, v);
+    }
+
+    /**
+     * 从顶点 s 到顶点 t 是否存在路径
+     *
+     * @return
+     */
+    public boolean isConnectedTo(int s, int t) {
+        G.validateVertex(s);
+        G.validateVertex(t);
+
+        return paths[s].isConnectedTo(t);
+    }
+
+    /**
+     * 返回从顶点 s 到顶点 t 的路径
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public Iterable<Integer> path(int s, int t) {
+        G.validateVertex(s);
+        G.validateVertex(t);
+
+        return paths[s].path(t);
+    }
+}
