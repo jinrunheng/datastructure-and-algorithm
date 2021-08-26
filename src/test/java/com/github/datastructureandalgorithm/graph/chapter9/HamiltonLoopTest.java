fd6eff051000b697d4478907245f4b7551648179
jinrunheng <1175088275@qq.com>
2021-08-26 12:06:15 +0800

    finish HamiltonLoopTest

---
+++ src/test/java/com/github/datastructureandalgorithm/graph/chapter9/HamiltonLoopTest.java
@@ -0,0 +1,26 @@
+package com.github.datastructureandalgorithm.graph.chapter9;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HamiltonLoopTest {
+
+    @Test
+    void result() {
+        Graph g = new Graph("/Users/macbook/Desktop/myProject/datastructure-and-algorithm/src/main/resources/graph/chapter9/g.txt");
+        HamiltonLoop hamiltonLoop = new HamiltonLoop(g);
+        System.out.println(hamiltonLoop.result());
+    }
+
+
+    @Test
+    /**
+     * 哈密尔顿问题
+     */
+    void result2() {
+        Graph g = new Graph("/Users/macbook/Desktop/myProject/datastructure-and-algorithm/src/main/resources/graph/chapter9/g2.txt");
+        HamiltonLoop hamiltonLoop = new HamiltonLoop(g);
+        System.out.println(hamiltonLoop.result());
+    }
+}
