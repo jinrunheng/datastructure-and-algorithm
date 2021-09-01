bc97ef8c01be59e145ad2ac58f16198d02267d53
jinrunheng <1175088275@qq.com>
2021-09-01 22:30:52 +0800

    优化了 Prim 算法，添加了优先级队列

--- src/main/java/com/github/datastructureandalgorithm/graph/chapter11/Prim.java
+++ src/main/java/com/github/datastructureandalgorithm/graph/chapter11/Prim.java
@@ -2,6 +2,8 @@ package com.github.datastructureandalgorithm.graph.chapter11;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Prim {
 
@@ -19,19 +21,22 @@ public class Prim {
         // true 表示为蓝色的顶点 false 表示为黑色的顶点
         boolean[] visited = new boolean[G.V()];
         visited[0] = true;
-        for (int i = 1; i < G.V(); i++) {
+        Queue<WeightedEdge> pq = new PriorityQueue<>(); // 最小堆
+        for (int w : G.adj(0))
+            pq.offer(new WeightedEdge(0, w, G.getWeight(0, w)));
 
-            WeightedEdge minEdge = new WeightedEdge(-1, -1, Integer.MAX_VALUE);
-
-            for (int v = 0; v < G.V(); v++)
-                if (visited[v])
-                    for (int w : G.adj(v))
-                        if (!visited[w] && G.getWeight(v, w) < minEdge.getWeight())
-                            minEdge = new WeightedEdge(v, w, G.getWeight(v, w));
+        while (!pq.isEmpty()) {
+            WeightedEdge minEdge = pq.remove();
+            if (visited[minEdge.getV()] && visited[minEdge.getW()])
+                continue;
 
             minimumSpanningTree.add(minEdge);
-            visited[minEdge.getV()] = true;
-            visited[minEdge.getW()] = true;
+
+            int p = visited[minEdge.getV()] ? minEdge.getW() : minEdge.getV();
+            visited[p] = true;
+            for (int w : G.adj(p))
+                if (!visited[w])
+                    pq.offer(new WeightedEdge(w, p, G.getWeight(w, p)));
         }
     }
 
