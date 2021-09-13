d88397fe2b513917b9e4f31cd0cfe116af780b6c
jinrunheng <1175088275@qq.com>
2021-09-14 07:15:53 +0800

    finish AVL Tree

--- src/main/java/com/github/datastructureandalgorithm/datastructure/AVL/AVLTree.java
+++ src/main/java/com/github/datastructureandalgorithm/datastructure/AVL/AVLTree.java
@@ -1,5 +1,11 @@
 package com.github.datastructureandalgorithm.datastructure.AVL;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 注释中有图，请勿格式化代码
+ */
 public class AVLTree<E extends Comparable<E>> {
     private Node root;
     private int size;
@@ -38,27 +44,27 @@ public class AVLTree<E extends Comparable<E>> {
     private int getBalanceFactor(Node node) {
         if (node == null)
             return 0;
-        return Math.abs(getHeight(node.left) - getHeight(node.right));
+        return getHeight(node.left) - getHeight(node.right);
     }
 
     /**
-     * @return 返回二分搜索树的根节点
+     * @return 返回 AVL 的根节点
      */
     public Node getRoot() {
         return root;
     }
 
     /**
-     * @param e 向二分搜索树中添加新的元素
+     * @param e 向 AVL 中添加新的元素
      */
     public void add(E e) {
         root = add(e, root);
     }
 
     /**
-     * @param e    向二分搜索树中新插入的节点
+     * @param e    向 AVL 中新插入的节点
      * @param node 当前比较的节点
-     * @return 返回二分搜索树的根节点
+     * @return 返回 AVL 的根节点
      */
     private Node add(E e, Node node) {
         if (node == null) {
@@ -76,15 +82,81 @@ public class AVLTree<E extends Comparable<E>> {
 
         // 计算平衡因子
         int balanceFactor = getBalanceFactor(node);
-        if (balanceFactor > 1)
-            System.out.println("unbalanced : " + balanceFactor);
 
+        // 平衡的维护
+
+        // LL
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0)
+            return rightRotate(node);
+
+        // RR
+        if(balanceFactor < -1 && getBalanceFactor(node.right) <= 0)
+            return leftRotate(node);
+
+        // LR
+        if(balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        // RL
+        if(balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
         return node;
     }
 
     /**
+     * 右旋转:
+     *
+     *            y                           x
+     *          /  \                       /     \
+     *        x    T4                    z        y
+     *      /  \         ========>     /  \     /  \
+     *    z     T3                   T1   T2  T3   T4
+     *  /  \
+     * T1  T2
+     */
+    private Node rightRotate(Node y){
+        Node x = y.left;
+        Node T3 = x.right;
+        x.right = y;
+        y.left = T3;
+
+        // 更新 height
+        y.height = Math.max(getHeight(y.left),getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left),getHeight(x.right)) + 1;
+        return x;
+    }
+
+    /**
+     * 左旋转:
+     *
+     *            y                           x
+     *          /  \                       /     \
+     *        T4    x                    y        z
+     *             /  \    ========>   /  \     /  \
+     *            T3   z             T4   T3  T1   T2
+     *               /  \
+     *              T1  T2
+     */
+    private Node leftRotate(Node y){
+        Node x = y.right;
+        Node T3 = x.left;
+        x.left = y;
+        y.right = T3;
+
+        // 更新 height
+        y.height = Math.max(getHeight(y.left),getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left),getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    /**
      * @param e 查找的元素 e
-     * @return 返回当前二分搜索树中是否包含元素 e
+     * @return 返回当前 AVL 中是否包含元素 e
      */
     public boolean contains(E e) {
         return contains(e, root);
@@ -93,7 +165,7 @@ public class AVLTree<E extends Comparable<E>> {
     /**
      * @param e    查找的元素 e
      * @param node 当前比较的节点
-     * @return 返回当前二分搜索树中是否包含元素 e
+     * @return 返回当前 AVL 中是否包含元素 e
      */
     private boolean contains(E e, Node node) {
         if (node == null) {
@@ -111,65 +183,59 @@ public class AVLTree<E extends Comparable<E>> {
         }
     }
 
+
     /**
-     * 二分搜索树的前序遍历
+     * 验证当前的 AVL 树是否满足二分搜索树的特性
+     *
+     * @return
      */
-    public void proOrder() {
-        preOrder(root);
+    public boolean isBST() {
+        ArrayList<E> list = new ArrayList<>();
+        inOrder(root, list);
+        for (int i = 1; i < list.size(); i++)
+            if (list.get(i - 1).compareTo(list.get(i)) > 0)
+                return false;
+        return true;
     }
 
     /**
-     * @param node 前序遍历以 node 为 根的二分搜索树
+     * @param node 中序遍历以 node 为 根的 AVL
      */
-    private void preOrder(Node node) {
+    private void inOrder(Node node, List<E> list) {
         if (node == null) {
             return;
         }
-        System.out.print(node.e + " ");
-        preOrder(node.left);
-        preOrder(node.right);
+        inOrder(node.left, list);
+        list.add((E) node.e);
+        inOrder(node.right, list);
     }
 
     /**
-     * 二分搜索树的中序遍历
+     * 验证当前的二叉树是否是一棵平衡的二叉树
+     *
+     * @return
      */
-    public void inOrder() {
-        inOrder(root);
+    public boolean isBalanced() {
+        return isBalanced(root);
     }
 
     /**
-     * @param node 中序遍历以 node 为 根的二分搜索树
+     * 验证当前的二叉树是否是一棵平衡的二叉树
+     *
+     * @param node
+     * @return
      */
-    private void inOrder(Node node) {
-        if (node == null) {
-            return;
-        }
-        inOrder(node.left);
-        System.out.println(node.e + " ");
-        inOrder(node.right);
+    private boolean isBalanced(Node node) {
+        if (node == null)
+            return true;
+        int balanceFactor = getBalanceFactor(node);
+        if (Math.abs(balanceFactor) > 1)
+            return false;
+        return isBalanced(node.left) && isBalanced(node.right);
     }
 
     /**
-     * 二分搜索树的后序遍历
-     */
-    public void postOrder() {
-        postOrder(root);
-    }
-
-    /**
-     * @param node 后序遍历以 node 为 根的二分搜索树
-     */
-    private void postOrder(Node node) {
-        if (node == null) {
-            return;
-        }
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.println(node.e + " ");
-    }
-
-    /**
-     * @return 返回二分搜索树的最小元素
+     * @return 返回 AVL 的最小元素
      */
     public E minimum() {
         if (size == 0) {
@@ -179,8 +245,8 @@ public class AVLTree<E extends Comparable<E>> {
     }
 
     /**
-     * @param node 返回以 node 为根的二分搜索树的最小值所在的节点
-     * @return 返回以 node 为根的二分搜索树的最小值所在的节点
+     * @param node 返回以 node 为根的 AVL 的最小值所在的节点
+     * @return 返回以 node 为根的 AVL 的最小值所在的节点
      */
     private Node minimum(Node node) {
         if (node.left == null) {
@@ -190,18 +256,18 @@ public class AVLTree<E extends Comparable<E>> {
     }
 
     /**
-     * @return 返回二分搜索树的最大元素
+     * @return 返回 AVL 的最大元素
      */
     public E maximum() {
         if (size == 0) {
-            throw new RuntimeException("BST is empty");
+            throw new RuntimeException("AVL is empty");
         }
         return (E) maximum(root).e;
     }
 
     /**
-     * @param node 返回以 node 为根的二分搜索树的最大值所在的节点
-     * @return 返回以 node 为根的二分搜索树的最大值所在的节点
+     * @param node 返回以 node 为根的 AVL 的最大值所在的节点
+     * @return 返回以 node 为根的 AVL 的最大值所在的节点
      */
     private Node maximum(Node node) {
         if (node.right == null) {
@@ -210,91 +276,79 @@ public class AVLTree<E extends Comparable<E>> {
         return maximum(node.right);
     }
 
-    /**
-     * @return 从二叉搜索树中删除最小值所在的节点，返回最小值
-     */
-    public E removeMin() {
-        E ret = minimum();
-        root = removeMin(root);
-        return ret;
-    }
-
-    /**
-     * @param node 删除掉以 node 为根的二分搜索树中的最小节点
-     * @return 返回删除节点后新的二分搜索树的根
-     */
-    private Node removeMin(Node node) {
-        if (node.left == null) {
-            Node rightNode = node.right;
-            node.right = null;
-            size--;
-            return rightNode;
-        }
-        node.left = removeMin(node.left);
-        return node;
-    }
-
-    /**
-     * @return 从二叉搜索树中删除最大值所在的节点，返回最大值
-     */
-    public E removeMax() {
-        E ret = maximum();
-        root = removeMax(root);
-        return ret;
-    }
-
-    /**
-     * @param node 删除掉以 node 为根的二分搜索树中的最大节点
-     * @return 返回删除节点后新的二分搜索树的根
-     */
-    private Node removeMax(Node node) {
-        if (node.right == null) {
-            Node leftNode = node.left;
-            node.left = null;
-            size--;
-            return leftNode;
-        }
-        node.right = removeMax(node.right);
-        return node;
-    }
-
     public void remove(E e) {
         root = remove(e, root);
     }
 
     private Node remove(E e, Node node) {
-        if (node == null) {
+        if (node == null)
             return null;
-        }
+
+        Node retNode;
         if (e.compareTo((E) node.e) < 0) {
             node.left = remove(e, node.left);
-            return node;
+            retNode = node;
         } else if (e.compareTo((E) node.e) > 0) {
             node.right = remove(e, node.right);
-            return node;
+            retNode = node;
         } else {
+            // 待删除节点的左子树为空
             if (node.left == null) {
                 Node rightNode = node.right;
                 node.right = null;
                 size--;
-                return rightNode;
+                retNode = rightNode;
             }
-            if (node.right == null) {
+            // 待删除的节点右子树为空时
+            else if (node.right == null) {
                 Node leftNode = node.left;
                 node.left = null;
                 size--;
-                return leftNode;
+                retNode = leftNode;
             }
-
-            // Hibbard Deletion
-            Node successor = minimum(node.right);
-            Node right = removeMin(node.right);
-            Node left = node.left;
-            successor.left = left;
-            successor.right = right;
-            node.left = null;
-            node.right = null;
-            return successor;
+            // 待删除的节点左右子树均不为空
+            else {// Hibbard Deletion
+                Node successor = minimum(node.right);
+                Node right = remove((E) node.right, successor);
+                Node left = node.left;
+                successor.left = left;
+                successor.right = right;
+                node.left = null;
+                node.right = null;
+                return successor;
+            }
         }
+
+        if(retNode == null)
+            return null;
+
+        // 更新 height
+        retNode.height = 1 + Math.max(getHeight(retNode.left), getHeight(retNode.right));
+
+        // 计算平衡因子
+        int balanceFactor = getBalanceFactor(retNode);
+
+        // 平衡的维护
+
+        // LL
+        if (balanceFactor > 1 && getBalanceFactor(retNode.left) >= 0)
+            return rightRotate(retNode);
+
+        // RR
+        if(balanceFactor < -1 && getBalanceFactor(retNode.right) <= 0)
+            return leftRotate(retNode);
+
+        // LR
+        if(balanceFactor > 1 && getBalanceFactor(retNode.left) < 0) {
+            retNode.left = leftRotate(retNode.left);
+            return rightRotate(retNode);
+        }
+
+        // RL
+        if(balanceFactor < -1 && getBalanceFactor(retNode.right) > 0) {
+            retNode.right = rightRotate(retNode.right);
+            return leftRotate(retNode);
+        }
+        return retNode;
     }
 }
---
+++ src/main/java/com/github/datastructureandalgorithm/graph/chapter12/Dijkstra.java
@@ -0,0 +1,12 @@
+package com.github.datastructureandalgorithm.graph.chapter12;
+
+import java.util.Arrays;
+
+public class Dijkstra {
+
+    private WeightedGraph G;
+    private int s;
+    private int[] dis;
+    private boolean[] visited;
+
+}
